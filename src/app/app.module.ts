0abad5c26ce318859382e973e870edad4b5ab157
Nidhi Chawla <s3633598@student.rmit.edu.au>
2017-08-10 00:00:32 +1000

    Landing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-
 import { appRouterModule } from "./app.routes";
-import { LandingPageComponent } from './landingPage.component';
-
 
 @NgModule({
   declarations: [
@@ -21,6 +17,7 @@ import { LandingPageComponent } from './landingPage.component';
      appRouterModule.routes
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
