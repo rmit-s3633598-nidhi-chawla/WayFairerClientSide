f656e63d777493455bb04554413d912b7219a806
Nidhi Chawla <s3633598@student.rmit.edu.au>
2017-08-19 14:48:59 +1000

    HTml changes

--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -3,13 +3,13 @@ import {Http} from '@angular/http';
 @Injectable()
 
 export class WayFairerServices {
-    url = "http://services.groupkt.com/country/get/";
+    url = " https://wayfairer-rest-api.herokuapp.com/";
      constructor(private http: Http) {
  };
     consumeWebService(endpoint:string) {
        console.log("End point:"+endpoint);
        console.log("Complete URL"+ this.url+endpoint);
-       this.url = this.url+endpoint;
+       //this.url = this.url+endpoint;
         this.http.get(this.url).subscribe(value =>{
             console.log(value)
            
